da0d20089641f083c0f0eccdbdc17b43e4377aa4
Narendra Sisodiya <narendra@billexo.com>
2018-05-24 15:12:41 +0530

    Download QR Code

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 $("#generateButton").click(function () {
   genQrCode()
-})
+});
 
 function genQrCode() {
   var accName = $("#accName").val();
   var accNumber = $("#accNumber").val();
   var accIFSC = $("#accIFSC").val();
+
+  //TODO - Show Error on empty Box.
   console.log(accName, accNumber, accIFSC);
   var str = "upi://pay?pa=" + accNumber + "@" + accIFSC +".ifsc.npci&pn=" + accName + "&cu=INR";
 
@@ -16,5 +18,22 @@ function genQrCode() {
     colorDark : "#000000",
     colorLight : "#ffffff",
     correctLevel : QRCode.CorrectLevel.H
+  });
+}
+
+$("#DownloadQrImage").click(function () {
+  DownloadQrImage();
 });
+
+function DownloadQrImage() {
+  html2canvas($("#A4Page"), {
+      letterRendering: 1,
+      allowTaint : true,
+  		onrendered: function (canvas) {
+        document.body.appendChild(canvas)
+  			// canvas.toBlob(function (blob) {
+  			// 	saveAs(blob, "UPI_QR_Code.png");
+  			// });
+  		}
+  });
 }
